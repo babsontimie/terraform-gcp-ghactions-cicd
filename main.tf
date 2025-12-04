4e95a8804a012f0a6beb546b08ecc9a6486f1a68
babsontimie <139099122+babsontimie@users.noreply.github.com>
2025-12-04 14:04:51 +0000

    Initialize Terraform with Google provider and bucket
    Add Terraform configuration for Google provider and storage bucket.

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = ">4.35.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+}
+
 resource "google_storage_bucket" "tee-auto-expire" {
   name          = "tee-auto-bucket"
   location      = "US"
