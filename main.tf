abb2fd903686f3cc1000f14ffb04baad8e3505e4
babsontimie <139099122+babsontimie@users.noreply.github.com>
2025-12-04 14:23:48 +0000

    Remove Google provider and bucket resource
    Removed provider and resource configuration for Google Cloud Storage bucket.

--- main.tf
+++ main.tf
@@ -1,20 +1,5 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = ">4.35.0"
-    }
-  }
-}
 
-provider "google" {
-  # Configuration options
-}
 
-resource "google_storage_bucket" "tee-auto-expire" {
-  name          = "tee-auto-bucket"
-  location      = "US"
-  force_destroy = true
-
-  public_access_prevention = "enforced"
+backend "local" {
+  path = "terraform.tfstate"
 }
