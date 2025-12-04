f79b6712caf5ab6c1595cb87b2b0da60d4dcafb0
Timie Babayeju <babsontimie@gmail.com>
2025-12-04 13:22:33 +0000

    Updated

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "google_storage_bucket" "tee-auto-expire" {
+  name          = "tee-auto-bucket"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
